7d9ea75e52f2d90ee725daa08aadddabd449d18b
oluwadare <wintan1418@gmail.com>
2021-05-06 15:20:57 +0100

    Add logic in backticks in menu.js

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -12,24 +12,27 @@ const html = `
             <div class = "menu-item">
                 <img src = "./src/assets/naija1-oka.png"></img>
                 <div class = "item-details">
-                 <p class="item-name">oka soup</p>
+                 <p class="item-name">Oka soup</p>
                  <p class="item-price">15$</p>
                 </div>
             </div>
             <div class = "menu-item">
                 <img src = "./src/assets/naija2.offals-pepper-soup.png"></img>
                 <div class = "item-details">
-                 <p class="item-name">Jollof rice</p>
+                 <p class="item-name">Pepper soup</p>
                  <p class="item-price">10$</p>
                 </div>
             </div>
             <div class = "menu-item">
-                <img src = "./src/assets/jollof-rice.png"></img>
+                <img src = "./src/assets/naija3-egusi.png"></img>
                 <div class = "item-details">
-                 <p class="item-name">Jollof rice</p>
+                 <p class="item-name">Egusi</p>
                  <p class="item-price">10$</p>
                 </div>
             </div>
         </div>
+        <p class="add-ons">With Soft Drink add 5$</p>
+`;
+menuNav.insertAdjacentHTML('beforeend', html);
 
-`
+return menuNav;
